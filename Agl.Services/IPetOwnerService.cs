c9610407ca933c92751832af099d8a1badaaeeaa
Rakibul <srhasan@yahoo.com>
2019-05-07 06:40:29 +1000

    Missing interfaces
    Missing interfaces

---
+++ Agl.Services/IPetOwnerService.cs
@@ -0,0 +1,15 @@
+﻿using Agl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agl.Services
+{
+    public interface IPetOwnerService
+    {
+        List<PetOwner> GetPetOwner();
+        List<GroupedPetOwner> GetGroupedAndSortedPetOwner(PetType petType, List<PetOwner> owners);
+    }
+}
