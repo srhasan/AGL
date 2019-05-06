5eeb3d54657d5123ce1505709edba45e026678b5
Rakibul <srhasan@yahoo.com>
2019-05-07 07:13:27 +1000

    Updated unit test with some mocked test

--- Agl.UniTest/Service/PetOwnerServiceTests.cs
+++ Agl.UniTest/Service/PetOwnerServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Agl.Model;
 using Agl.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,9 +10,41 @@ namespace Agl.Console.Service.Tests
     [TestClass()]
     public class PetOwnerServiceTests
     {
+        private string mockedResponse = "[{'name':'Bob','gender':'Male','age':23,'pets':[{'name':'Garfield','type':'Cat'},{'name':'Fido','type':'Dog'}]},{'name':'Jennifer','gender':'Female','age':18,'pets':[{'name':'Garfield','type':'Cat'}]},{'name':'Steve','gender':'Male','age':45,'pets':null},{'name':'Fred','gender':'Male','age':40,'pets':[{'name':'Tom','type':'Cat'},{'name':'Max','type':'Cat'},{'name':'Sam','type':'Dog'},{'name':'Jim','type':'Cat'}]},{'name':'Samantha','gender':'Female','age':40,'pets':[{'name':'Tabby','type':'Cat'}]},{'name':'Alice','gender':'Female','age':64,'pets':[{'name':'Simba','type':'Cat'},{'name':'Nemo','type':'Fish'}]}]";
 
         [TestMethod()]
-        public void DisplayPetsGroupedByOwnerGender_NullList_Should_Pass()
+        public void GetPetOwner_ValidClientResponse_Should_Return_Correct_Number_Of_Result()
+        {
+            // Arrange
+            var mockedClientService = new Mock<IWebClientService>();
+            mockedClientService.Setup(x => x.GetPetOwnerJson()).Returns(mockedResponse);
+            var service = new PetOwnerService(mockedClientService.Object);
+
+            //Act
+            var result = service.GetPetOwner();
+            
+            //Assert
+            Assert.IsTrue(result.Count == 6);
+        }
+
+        [TestMethod()]
+        public void GetGroupedAndSortedPetOwner_ListShouldBeInOrder()
+        {
+            // Arrange
+            var mockedClientService = new Mock<IWebClientService>();
+            mockedClientService.Setup(x => x.GetPetOwnerJson()).Returns(mockedResponse);
+            var service = new PetOwnerService(mockedClientService.Object);
+
+            //ACT
+            var unsortedOwner = service.GetPetOwner();
+            var result = service.GetGroupedAndSortedPetOwner(PetType.Cat, unsortedOwner);
+
+            // Assert
+            Assert.IsTrue(result.First().Pets.First() == "Garfield", "Pet names should be sorted");
+        }
+
+        [TestMethod()]
+        public void GetGroupedAndSortedPetOwner_NullList_Should_Pass()
         {
             var service = new PetOwnerService(null);
             var result = service.GetGroupedAndSortedPetOwner(PetType.Cat, null);
@@ -19,27 +52,14 @@ namespace Agl.Console.Service.Tests
         }
 
         [TestMethod()]
-        public void DisplayPetsGroupedByOwnerGender_EmptyList_Should_Pass()
+        public void GetGroupedAndSortedPetOwner_EmptyList_Should_Pass()
         {
-
             var service = new PetOwnerService(null);
             var result = service.GetGroupedAndSortedPetOwner(PetType.Cat, new List<PetOwner>());
             Assert.IsTrue(result != null);
         }
 
-        [TestMethod()]
-        public void DisplayPetsGroupedByOwnerGender_ListShouldBeInOrder()
-        {
-            var service = new PetOwnerService(null);
-            var unsortedOwner = new List<PetOwner> {
-                new PetOwner { Age = 20, Gender = GenderType.Female, Name = "User1", Pets = new List<Pet> { new Pet {Name = "Scrappy", Type = PetType.Cat }, new Pet { Name = "Tom", Type = PetType.Cat } } },
-                new PetOwner { Age = 20, Gender = GenderType.Male, Name = "User2", Pets = new List<Pet> { new Pet {Name = "Tom", Type = PetType.Cat }, new Pet { Name = "Alfie", Type = PetType.Cat } } },
-                new PetOwner { Age = 22, Gender = GenderType.Female, Name = "User3", Pets = new List<Pet> { new Pet {Name = "Boa", Type = PetType.Cat }, new Pet { Name = "Polo", Type = PetType.Dog } } }
-            };
-            var result = service.GetGroupedAndSortedPetOwner(PetType.Cat, unsortedOwner);
-            Assert.IsTrue(result.Count == 2);
-            Assert.IsTrue(result.First().Pets.First() == "Alfie");
-        }
+        
 
         
     }
